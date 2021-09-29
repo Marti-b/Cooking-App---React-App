08ecab7df91aee3ccc5241a4291e4fb3602c0242
Balazs <martikanbalazs@gmail.com>
2021-09-29 12:52:12 +0200

    Enable adding title, desc, ingredients to recipes

--- src/AddRecipe.js
+++ src/AddRecipe.js
@@ -2,27 +2,60 @@ import { useState } from 'react';
 
 
 function AddRecipe(props) {
-    const [input, setInput] = useState("");
+
+    const {addRecipe} = props;
+
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+    const [ingredients, setIngredients] = useState("");
     console.log("Props of Addrecipe", props);
 
     return(
-        <>
-        <h3>Add Recipe to the list</h3>
-            <input 
-                type="text" 
-                value={input} 
-                onChange={(event) => {
-                    setInput(event.target.value);
-                }} 
-            />
-            <button 
-                onClick={() => {
-                    props.addRecipe(input);
-                    setInput("")
-          }}>
-              Add recipe
-              </button>
-        </>
-    )
+    
+        <div className="add-recipes">
+            <h3>Add Recipe to the list</h3>
+            <div>
+                <label htmlFor="title">Title</label>
+                <input
+                    name="title"
+                    id="title"
+                    onChange={(event) => setTitle(event.target.value)}
+                    type="text"
+                />
+            </div>
+        <div>
+                <label htmlFor="description">Description</label>
+                <textarea
+                    name="description"
+                    id="description"
+                    onChange={(event) => setDesc(event.target.value)}></textarea>
+        </div>
+        <div>
+                <label htmlFor="title">Ingredients</label>
+                <input
+                    name="ingredients"
+                    id="ingredients"
+                    onChange={(event) => setIngredients(event.target.value)}
+                    type="text"
+                />
+        </div>
+            <button
+            type="submit"
+            onClick= {(event) =>{
+                const ingredientsArray = ingredients
+                    // Split on comma
+                    .split(",")
+                    // Trim extraneous whitespace
+                    .map((item) => item.trim())
+                    // Filter out empty strings (i.e. from extra commas in the input)
+                    .filter((item) => item !== "");
+
+                addRecipe(title, desc, ingredientsArray)
+            }}>
+                Add Recipe
+            </button>   
+        </div>
+     
+    );
 }
 export default AddRecipe;
